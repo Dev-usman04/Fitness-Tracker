c98ba845de444ef2f8b6970eea5b453097e122cd
DevDre <bighalo77@gmail.com>
2025-07-12 20:04:56 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
